15165114170e3af99e2b797f4ef7fc580450ae69
gregorio <gregorio.gerardi@reflektion.com>
2022-04-30 01:07:15 -0300

    some more minor tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,16 @@ import styles from '../styles/Home.module.css'
 import logo from '../public/logo.png'
 import background from '../public/background_large.jpeg'
 import whatsAppLogo from '../public/whatsapp.png'
+import instagramLogo from '../public/instagram.png'
+import facebookLogo from '../public/facebook.png'
+import mapsLogo from '../public/pin.png'
 import glutenFreeLogo from '../public/gluten_free_logo.png'
 import menu from '../public/menu'
 import {Fragment} from "react";
 
 const Home: NextPage = () => {
     return (
-        <div style={{backgroundImage: `url(${background.src})`}} className={styles.container}>
+        <div className={styles.container}>
             <Head>
                 <title>Punto Café Necochea</title>
                 <meta name="description" content="Cafeteria. Desayunos, almuerzos y meriendas"/>
@@ -27,9 +30,9 @@ const Home: NextPage = () => {
                         Punto Café Necochea
                     </h1>
                 </div>
-                <div className={styles.menu}>
+                <div className={styles.menu} style={{backgroundImage: `url(${background.src})`}}>
                     {Object.entries(menu).map(([section, items]) => (
-                        <Fragment key={section}>
+                        <div key={section} className={styles.section}>
                             <h2 className={styles.sectionHeader}>{section}</h2>
                             <div className={styles.grid}>
                                 {items.map(({title, price, description, glutenFree}) => (
@@ -53,17 +56,41 @@ const Home: NextPage = () => {
                                     </div>
                                 ))}
                             </div>
-                        </Fragment>
+                        </div>
                     ))}
                 </div>
+                <footer className={styles.footer}>
+                    <span className={styles.footerTitle}>Punto Café Necochea</span>
+                    <iframe
+                        className={styles.map}
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1560.123784489275!2d-58.73831388227598!3d-38.55110950475783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x958f97859ecf9c51%3A0x867b556122bfc133!2sPunto%20Caf%C3%A9!5e0!3m2!1ses!2sar!4v1651288021145!5m2!1ses!2sar"
+                        width="100%" height="200px" loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"/>
+                    <div className={styles.socialMediaContainer}>
+                        <a className={styles.socialMedia} target="_blank" rel="noreferrer"
+                           href="https://wa.me/5492262677257">
+                            <Image src={whatsAppLogo} alt="Whats App Logo" height="24px" width="24px"/>
+                            <span className={styles.socialMediaLabel}>2262 677257</span>
+                        </a>
+                        <a className={styles.socialMedia} target="_blank" rel="noreferrer"
+                           href="https://www.instagram.com/puntocafenecochea/">
+                            <Image src={instagramLogo} alt="Instagram Logo" height="24px" width="24px"/>
+                            <span>@puntocafenecochea</span>
+                        </a>
+                        <a className={styles.socialMedia} target="_blank" rel="noreferrer"
+                           href="https://www.facebook.com/puntocafenecochea">
+                            <Image src={facebookLogo} alt="Facebook Logo" height="24px" width="24px" layout="fixed"/>
+                            <span>@puntocafenecochea</span>
+                        </a>
+                        <a className={styles.socialMedia} target="_blank" rel="noreferrer"
+                           href="https://goo.gl/maps/pPgkzK29d8Sh6BqbA">
+                            <Image src={mapsLogo} alt="Maps Logo" height="24px" width="18px" layout="fixed"/>
+                            <span>60 #2596</span>
+                        </a>
+                    </div>
+                </footer>
             </main>
-            <footer className={styles.footer}>
-                <span>Punto Café Necochea</span>
-                <a className={styles.flexRow} target="_blank" rel="noreferrer" href="https://wa.me/5492262677257">
-                    <Image src={whatsAppLogo} alt="Whats App Logo" height="24px" width="24px"/>
-                    <span>2262 677257</span>
-                </a>
-            </footer>
+
         </div>
     )
 }
